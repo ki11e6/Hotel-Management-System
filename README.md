2168a22edab36a93f11ec198eb1611c9d18c05bf
Sharath.S <sharath.surendran.m@gmail.com>
2024-06-19 18:41:56 +0530

    styled component

--- README.md
+++ README.md
@@ -12,6 +12,17 @@ This is internal hotal management system used by employees or owner to manage bo
 - Dashboard
 - Additional settings - Dark mode, etc..
 
+## Stack using
+
+| Technology              | Image                      | Description          |
+| ----------------------- | -------------------------- | -------------------- |
+| Routing                 | ![Alt text](url-to-image1) | Description of Tech1 |
+| Styling                 | ![Alt text](url-to-image2) | Description of Tech2 |
+| Remote state management | ![Alt text](url-to-image3) | Description of Tech3 |
+| UI state management     | ![Alt text](url-to-image3) | Description of Tech3 |
+| Form management         | ![Alt text](url-to-image3) | Description of Tech3 |
+| Tech3                   | ![Alt text](url-to-image3) | Description of Tech3 |
+
 ## Installation
 
 Provide step by step series of examples and explanations about how to get a development env running.
--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,46 @@
+import styled from "styled-components";
+import GlobalStyles from "./styles/GlobalStyles";
+
+const H1 = styled.h1`
+    font-size: 30px;
+    font-weight: 600;
+    background-color: green;
+`;
+const Button = styled.button`
+    font-size: 1.4rem;
+    padding: 1.2rem 1.6rem;
+    font-weight: 500;
+    border: none;
+    border-radius: 7px;
+    background-color: purple;
+    color: white;
+    cursor: pointer;
+    margin: 20px;
+`;
+
+const Input = styled.input`
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    padding: 0.8rem 1.2rem;
+`;
+
+const StyledApp = styled.main`
+    background-color: orangered;
+    padding: 20px;
+`;
+
 function App() {
-  return <h1>Home</h1>
+    return (
+        <>
+            <GlobalStyles />
+            <StyledApp>
+                <H1>The Wild Oasis</H1>
+                <Button>Check in</Button>
+                <Button>Check out</Button>
+                <Input type="text" placeholder="Enter your name" />
+            </StyledApp>
+        </>
+    );
 }
 
-export default App
+export default App;
