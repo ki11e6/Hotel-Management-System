c79f71f3cc83a10888dc396ac65abec175f7760c
Sharath.S <sharath.surendran.m@gmail.com>
2024-07-24 04:00:24 +0530

    chart

--- src/features/dashboard/DashboardLayout.jsx
+++ src/features/dashboard/DashboardLayout.jsx
@@ -4,6 +4,7 @@ import useRecentStays from './useRecentStays';
 import Spinner from '../../ui/Spinner';
 import Stats from './Stats';
 import useCabins from '../cabins/useCabins';
+import SalesChart from './SalesChart';
 const StyledDashboardLayout = styled.div`
   display: grid;
   grid-template-columns: 1fr 1fr 1fr 1fr;
@@ -33,7 +34,7 @@ const DashboardLayout = () => {
       />
       <div>Today&apos;s activity</div>
       <div>Chart stay duration</div>
-      <div>Chat sales</div>
+      <SalesChart bookings={bookings} numDays={numDays} />
     </StyledDashboardLayout>
   );
 };
